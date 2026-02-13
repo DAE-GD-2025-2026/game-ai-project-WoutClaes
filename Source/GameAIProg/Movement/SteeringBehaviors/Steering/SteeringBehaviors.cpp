7765c2caeeb73bf9060ca7286bed334734c0490f
WoutClaes <wout.claes@student.howest.be>
2026-02-13 21:36:41 +0100

    Face, Pursuit and Evade
    Added the behaviours face, pursuit and evade. Face has not been tested yet because it seems that the dropdown to select the behaviours does not include this. This is something that still needs to be added with the debug rendering.

--- Source/GameAIProg/Movement/SteeringBehaviors/Steering/SteeringBehaviors.cpp
+++ Source/GameAIProg/Movement/SteeringBehaviors/Steering/SteeringBehaviors.cpp
@@ -59,26 +59,80 @@ SteeringOutput Arrive::CalculateSteering(float DeltaT, ASteeringAgent& Agent)
 //*******
 SteeringOutput Face::CalculateSteering(float DeltaT, ASteeringAgent& Agent)
 {
-	return SteeringOutput();
+	SteeringOutput steering{};
+	
+	FVector2D toTarget = Target.Position - Agent.GetPosition();
+	
+	if (toTarget.IsNearlyZero())
+	{
+		steering.AngularVelocity = 0.f;
+		return steering;
+	}
+	
+	float desiredAngle = atan2(toTarget.Y, toTarget.X);
+	float currentAngle = Agent.GetRotation();
+	float angleDifference = desiredAngle - currentAngle;
+	
+	steering.AngularVelocity = angleDifference;
+	
+	return steering;
 }
 
 //PURSUIT
 //*******
 SteeringOutput Pursuit::CalculateSteering(float DeltaT, ASteeringAgent& Agent)
 {
-	return SteeringOutput();
+	SteeringOutput steering{};
+	
+	FVector2D toTarget = Target.Position - Agent.GetPosition();
+	float distance = toTarget.Length();
+	
+	if (toTarget.IsNearlyZero())
+	{
+		steering.LinearVelocity = toTarget;
+		return steering;
+	}
+	
+	float speed = Agent.GetMaxLinearSpeed();
+	float predictionTime = distance / speed;
+
+	FVector2D predictedPos = Target.Position + Target.LinearVelocity * predictionTime;
+
+	steering.LinearVelocity = predictedPos - Agent.GetPosition();
+	
+	return steering;
 }
 
 //EVADE
 //*******
 SteeringOutput Evade::CalculateSteering(float DeltaT, ASteeringAgent& Agent)
 {
-	return SteeringOutput();
+	SteeringOutput steering{};
+	
+	FVector2D toTarget = Target.Position - Agent.GetPosition();
+	float distance = toTarget.Length();
+	
+	if (toTarget.IsNearlyZero())
+	{
+		steering.LinearVelocity = Agent.GetPosition() - Target.Position;
+		return steering;
+	}
+	
+	float speed = Agent.GetMaxLinearSpeed();
+	float predictionTime = distance / speed;
+
+	FVector2D predictedPos = Target.Position + Target.LinearVelocity * predictionTime;
+
+	steering.LinearVelocity = Agent.GetPosition() - predictedPos;
+	
+	return steering;
 }
 
 //WANDER
 //*******
 SteeringOutput Wander::CalculateSteering(float DeltaT, ASteeringAgent& Agent)
 {
-	return SteeringOutput();
+	SteeringOutput steering{};
+	
+	return steering;
 }
